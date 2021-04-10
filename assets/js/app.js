ae4d391614b2a9fdc77108a8929d646754a7f36a
Ayush Shah <shahayush2507@gmail.com>
2021-04-11 00:23:21 +0530

    added "id" to firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@ const send = document.querySelector(".contact__button");
 const thank_you = document.querySelector(".thank-you-alert-box");
 const invalid_email = document.querySelector(".email-alert-box");
 const invalid_msg = document.querySelector(".msg-alert-box");
+const invalid_name = document.querySelector(".name-alert-box");
 
 var firebaseConfig = {
     apiKey: "AIzaSyARrvQP8Hjrx4Wp6GtTWYQBlhrDeCtONMU",
@@ -20,13 +21,15 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 
-function ValidateEmail(mail) 
-{
- if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail))
-  {
-    return (true)
-  }
-    return (false)
+function ValidateEmail(inputText) {
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(inputText.value?.match(mailformat)) return true;
+    return false;
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 function wait(ms) {
@@ -53,21 +56,38 @@ function ValidateMsg(msg) {
     return false;
 }
 
+function randomString() {
+    var len = Math.random()+"";
+    var str = "";
+    for(var i=0;i<len.length;i++) {
+        if(len[i]!='.') str+=len[i];
+    }
+    return str;
+}
+
 send.addEventListener('click', () => {  
     var delay = 2000;
-    var email_ok = ValidateEmail(sender_email?.value), msg_ok = ValidateMsg(msg_value?.value);
-    if(!email_ok) {
+    var email_ok = validateEmail(sender_email?.value), msg_ok = ValidateMsg(msg_value?.value), name_ok = ValidateMsg(sender_name?.value);
+    if(!name_ok) {
+        pop(invalid_name);
+        setTimeout(() => {
+            rem(invalid_name);
+        }, delay);
+    } else if(!email_ok) {
         pop(invalid_email);
         setTimeout(() => {
             rem(invalid_email);
         }, delay);
+        sender_email.value = "";
     } else if(!msg_ok) {
         pop(invalid_msg);
         setTimeout(() => {
             rem(invalid_msg);
         }, delay);
     } else {
-        firebase.database().ref("User").set({
+        var path = randomString();
+        console.log(path);
+        firebase.database().ref(path).set({
             name: sender_name?.value,
             email: sender_email?.value, 
             message: msg_value?.value
